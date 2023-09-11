babf3877270d7f64b859387e78c22fa2a57829cd
tomgiagtz <tom.giagtzoglou@gmail.com>
2023-09-11 14:03:08 -0400

    add monument animation, basic player controller for pebble

--- Assets/Code/Gameplay/PebbleController.cs
+++ Assets/Code/Gameplay/PebbleController.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PebbleController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class PebbleController : MonoBehaviour {
+    private Rigidbody rb;
+    private Vector3 torqueInput;
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnMove(InputValue inputVec) {
+        Vector2 vec = inputVec.Get<Vector2>();
+        if (vec.magnitude == 0) {
+            torqueInput = Vector3.zero;
+            return;
+        }
+        Debug.Log("move: " + vec);
+        // Vector3 forceVec = new Vector3(vec.x, 0, vec.y);
+        // rb.AddForce(forceVec * 100, ForceMode.Acceleration);
+        torqueInput = new Vector3(vec.y, 0, -vec.x); // This will rotate the object around its Y-axis.
+        // torqueVector = Camera.main.transform.rotation * torque;
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    public void OnJump() {
+        Debug.Log("yump");
+    }
+
+    private void FixedUpdate() {
+        Vector3 torqueVector = Camera.main.transform.rotation * torqueInput;
+        rb.AddTorque(torqueVector * 100, ForceMode.Force);
         
+        //gravity
+        rb.AddForce(Vector3.down * 50, ForceMode.Acceleration);
     }
 }
