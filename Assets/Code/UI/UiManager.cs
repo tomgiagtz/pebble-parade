9b0d082e1ddded8bfd80fa036c5ece7c321e1737
SamArcaro <62896598+SamArcaro@users.noreply.github.com>
2023-09-13 13:35:46 -0400

    Added ScriptableSingleton & UiManager class

---
+++ Assets/Code/UI/UiManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+	
+public class UiManager : MonoBehaviour {
+	
+	public new GameObject gameObject;
+	
+	public RectTransform rect;
+	
+	public virtual void Start() {
+		this.SetActive(false);
+	}
+	
+	public virtual void SetActive(bool toggle) {
+		this.gameObject.SetActive(toggle);
+	}
+}
