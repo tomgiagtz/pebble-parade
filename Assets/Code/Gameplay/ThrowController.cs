3e46091395c3cabbf8ea112f2a2172ebfb79ebe6
Teych <tholub99@gmail.com>
2023-09-17 16:52:08 -0400

    Update throw projector to auto adjust to cornered ground

--- Assets/Code/Gameplay/ThrowController.cs
+++ Assets/Code/Gameplay/ThrowController.cs
@@ -12,19 +12,42 @@ public class ThrowController : MonoBehaviour
 
     [Header("Aim Params")]
     public LayerMask hitLayer;
-    public float playerOffset = 1f;
-    public float maxAimDistance = 5f;
+    public float playerOffset = 2f;
+    public float maxAimDistance = 30f;
 
     
 
     bool isAiming;
-    float projSize = 0.8f;
+    float projSize = 0.5f;
 
     Vector3 aimPoint;
     Vector3 aimNormal;
     Vector3 aimNormalRotate1;
     Vector3 aimNormalRotate2;
     Vector3[] projPoints;
+
+    private void Update()
+    {
+        if (isAiming)
+        {
+            RaycastHit hit;
+            Camera cam = Camera.main;
+            Vector3 direction = ((gameObject.transform.position + cam.transform.up * playerOffset) - cam.transform.position);
+            if (Physics.Raycast(Camera.main.transform.position, direction, out hit, maxAimDistance, hitLayer))
+            {
+                aimPoint = hit.point;
+                aimNormal = hit.normal;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (isAiming)
+        {
+            projector.transform.position = aimPoint;
+            projector.transform.forward = CalcAvgNormal();
+        }
+    }
     public void OnToggleAim(InputAction.CallbackContext context)
     {
         if (context.started) ToggleAimOn();
@@ -44,79 +67,43 @@ public class ThrowController : MonoBehaviour
         Debug.Log("Aiming Off");
     }
 
-    private void Update()
-    {
-        if(isAiming)
-        {
-            RaycastHit hit;
-            Camera cam = Camera.main;
-            Vector3 direction = ((gameObject.transform.position + cam.transform.up * playerOffset) - cam.transform.position);
-            if (Physics.Raycast(Camera.main.transform.position, direction, out hit, Mathf.Infinity, hitLayer))
-            {
-                aimPoint = hit.point;
-                aimNormal = hit.normal;
-            }
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if(isAiming)
-        {
-            projector.transform.position = aimPoint;
-            projector.transform.forward = CalcAvgNormal();
-        }
-    }
-
     Vector3 CalcAvgNormal()
     {
+
+
         Camera cam = Camera.main;
-        Vector3 cross = Vector3.Cross(aimNormal, Vector3.zero).normalized;
-        Vector3 vertVec = (Quaternion.AngleAxis(90, Vector3.forward) * aimNormal).normalized;
-        Vector3 horizVec = (Quaternion.AngleAxis(90, Vector3.right) * aimNormal).normalized;
-        if(Vector3.Dot(aimNormal, vertVec) >= 0.95f)
-        {
-            vertVec = (Quaternion.AngleAxis(90, Vector3.up) * aimNormal).normalized;
-            horizVec = (Quaternion.AngleAxis(90, Vector3.right) * aimNormal).normalized;
-        }
-        else if(Vector3.Dot(aimNormal, horizVec) >= 0.95f)
-        {
-            vertVec = (Quaternion.AngleAxis(90, Vector3.up) * aimNormal).normalized;
-            horizVec = (Quaternion.AngleAxis(90, Vector3.forward) * aimNormal).normalized;        
-        }
+        Vector3 normalVec = -cam.transform.forward;
+        Vector3 rightVec = cam.transform.right;
+        Vector3 upVec = cam.transform.up;
 
-        aimNormalRotate1 = vertVec;
-        aimNormalRotate2 = horizVec;
-        Vector3 p1 = aimPoint - (horizVec * projSize) + (vertVec * projSize) + (aimNormal * projSize);
-        Vector3 p2 = aimPoint + (horizVec * projSize) + (vertVec * projSize) + (aimNormal * projSize);
-        Vector3 p3 = aimPoint - (horizVec * projSize) - (vertVec * projSize) + (aimNormal * projSize);
-        Vector3 p4 = aimPoint + (horizVec * projSize) - (vertVec * projSize) + (aimNormal * projSize);
+        aimNormalRotate1 = rightVec;
+        aimNormalRotate2 = upVec;
+        Vector3 p1 = aimPoint - (rightVec * projSize) + (upVec * projSize) + (normalVec * projSize);
+        Vector3 p2 = aimPoint + (rightVec * projSize) + (upVec * projSize) + (normalVec * projSize);
+        Vector3 p3 = aimPoint - (rightVec * projSize) - (upVec * projSize) + (normalVec * projSize);
+        Vector3 p4 = aimPoint + (rightVec * projSize) - (upVec * projSize) + (normalVec * projSize);
 
-        Vector3 p5 = aimPoint - (horizVec * projSize) + (vertVec * projSize) - (aimNormal * projSize);
-        Vector3 p6 = aimPoint + (horizVec * projSize) + (vertVec * projSize) - (aimNormal * projSize);
-        Vector3 p7 = aimPoint - (horizVec * projSize) - (vertVec * projSize) - (aimNormal * projSize);
-        Vector3 p8 = aimPoint + (horizVec * projSize) - (vertVec * projSize) - (aimNormal * projSize);
-        projPoints = new Vector3[] {p1, p2, p3, p4, p5, p6, p7, p8};
+        projPoints = new Vector3[] {p1, p2, p3, p4};
 
-        Vector3 direction = cam.transform.forward;
         Vector3 normalAvg = Vector3.zero;
+        int avgCount = 0;
         foreach(Vector3 point in projPoints)
         {
             RaycastHit hit;
-            if (Physics.Raycast(point, direction, out hit, 5f, hitLayer))
+            Vector3 direction = (point - cam.transform.position);
+            if (Physics.Raycast(cam.transform.position, direction, out hit, maxAimDistance * 1.5f, hitLayer))
             {
                 normalAvg += hit.normal;
+                avgCount++;
             }
         }
-        return -(normalAvg * (1f / projPoints.Length));
+        return -(normalAvg * (1f / avgCount));
     }
 
     public void OnThrow(InputAction.CallbackContext context)
     {
         if (!isAiming || !context.started) return;
         Debug.Log("Throwing");
-
-
     }
 
     private void OnDrawGizmos()
@@ -134,12 +121,13 @@ public class ThrowController : MonoBehaviour
             Gizmos.DrawRay(aimPoint, aimNormalRotate2);
         }
 
-        if(projector.gameObject.activeSelf)
+        if(projector.gameObject.activeSelf && projPoints != null)
         {
             Gizmos.color = Color.red;
             foreach (Vector3 point in projPoints)
             {
                 Gizmos.DrawSphere(point, 0.1f);
+                Gizmos.DrawRay(Camera.main.transform.position, (point - Camera.main.transform.position).normalized * 100f);
             }
         }
     }
