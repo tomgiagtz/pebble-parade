7a05b004558385d94551351f002388b6cf4156b6
tomgiagtz <tom.giagtzoglou@gmail.com>
2023-09-25 14:37:09 -0400

    basic parade w collectable rocks

--- Assets/Code/AI/ParadeController.cs
+++ Assets/Code/AI/ParadeController.cs
@@ -8,8 +8,12 @@ public class ParadeController : MonoBehaviour {
     public Transform leader;
     public float targetUpdateInterval = 1f;
     
+    public List<Mesh> possibleMeshes = new List<Mesh>();
+    private List<Mesh> usedMeshes = new List<Mesh>();
+    
     private List<ParadeMember> members = new List<ParadeMember>();
     private Vector3 targetPosition = Vector3.zero;
+    private Vector3 prevTargetPosition = Vector3.zero;
     
     public ParadeControllerDebug debug;
     
@@ -18,22 +22,51 @@ public class ParadeController : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        targetPosition = leader.position;
+        
     }
 
     public void AddMember(GameObject member) {
         if (!member.CompareTag("ParadeMember")) return;
+
+        Mesh newMesh = GetRandomMesh();
         
-        Debug.Log("Adding member");
+        
+        // Debug.Log("Adding member");
         ParadeMember newParadeMember = member.GetComponent<ParadeMember>();
         members.Add(newParadeMember);
-        newParadeMember.Init();
+        newParadeMember.Init(newMesh);
         member.transform.SetParent(this.transform);
     }
-    
+
+    private Mesh GetRandomMesh() {
+        if (possibleMeshes.Count == 0) {
+            possibleMeshes.AddRange(usedMeshes.ToArray());
+            usedMeshes.Clear();
+            
+        }
+        
+        //get a new mesh
+        int newMeshIndex = UnityEngine.Random.Range(0, possibleMeshes.Count);
+        Mesh newMesh = possibleMeshes[newMeshIndex];
+        // add it to used meshes, remove it from possible meshes
+        usedMeshes.Add(newMesh);
+        possibleMeshes.RemoveAt(newMeshIndex);
+
+        return newMesh;
+    }
+
     private void UpdateMemberPositions() {
+        prevTargetPosition = targetPosition;
+        targetPosition = leader.position;
+        
+        // stop updating positions when player not moving
+        if (Vector3.Distance(prevTargetPosition, targetPosition) < 1f)  return;
+        
         for (int i = 0; i < members.Count; i++) {
-            members[i].SetTarget(targetPosition);
+            Vector3 randomDirection = new Vector3(UnityEngine.Random.Range(-1f, 1f), 0, UnityEngine.Random.Range(-1f, 1f)).normalized;
+            Vector3 randomPosition = targetPosition + randomDirection * UnityEngine.Random.Range(2f, 5f);
+            
+            members[i].SetTarget(randomPosition);
         }
     }
     
--- Assets/Code/AI/ParadeMember.cs
+++ Assets/Code/AI/ParadeMember.cs
@@ -8,57 +8,63 @@ public class ParadeMember : MonoBehaviour {
     
     NavMeshAgent agent;
     Rigidbody rb;
+
+    [Header("Follow")]
+    public float minDistance = 2f;
+    public float maxDistance = 20f;
     
-    Vector3 currentTarget;
+    Vector3 currTarget;
     private Vector3 currDirection;
     ParadeMemberState state = ParadeMemberState.Moving;
+    
+    
     
     
     public ParadeMemberDebug debug;
     private void Start() {
         rb = GetComponent<Rigidbody>();
-        agent = GetComponent<NavMeshAgent>();
-        
-        agent.updatePosition = false;
-        agent.updateRotation = false;
     }
 
     private void FixedUpdate() {
-        // if (state == ParadeMemberState.Moving) {
-        //     rb.AddTorque(transform.right * 2500f, ForceMode.Force);
-        // }
-        // agent.transform.position = rb.position;
-        // if (!agent.isOnNavMesh) {
-        //     // agent.Warp(agent.nextPosition);
-        //     Debug.LogWarning("Agent not on navmesh");
-        // }
-        // agent.velocity = rb.velocity;
-        // if (agent.hasPath) {
-        //     // Get the next position on the path
-        //     Vector3 nextPosition = agent.steeringTarget;
-        //
-        //     // Calculate the direction to the next position
-        //     Vector3 moveDirection = (nextPosition - transform.position).normalized;
-        //     currDirection = moveDirection;
-        //     currDirection.y = 0f;
-        //     currDirection = currDirection.normalized;
-        //     
-        //     
-        //     rb.AddTorque(moveDirection * 10f, ForceMode.Impulse);
-        //     // Move the Rigidbody towards the next position
-        //     // rb.AddTorque(transform.right * Quaternion., ForceMode.Force);
-        //      agent.Warp(rb.position);
-        // } else {
-        //     // rb.position = agent.nextPosition;
-        // }
+        if (state == ParadeMemberState.Teleporting) {
+            rb.velocity = Vector3.zero;
+            rb.MovePosition(currTarget);
+            rb.AddForce(Vector3.up * 30f, ForceMode.Force);
+            state = ParadeMemberState.Moving;
+        }
+        
+        float distanceFromTarget = Vector3.Distance(this.transform.position, currTarget);
+        if (distanceFromTarget < minDistance) {
+            state = ParadeMemberState.Idle;
+        } else if (distanceFromTarget < maxDistance) {
+            state = ParadeMemberState.Moving;
+        } else {
+            state = ParadeMemberState.Teleporting;
+        }
         
         if (state == ParadeMemberState.Moving) {
-               rb.AddTorque(transform.right * 2500f, ForceMode.Force);
+               RotateTowardsTarget();
+               rb.AddTorque(torque * 25000f, ForceMode.Force);
         }
+
+        
+    }
+    Vector3 torque;
+    private void RotateTowardsTarget() {
+        currDirection = (currTarget - transform.position);
+        currDirection.y = 0;
+        currDirection.Normalize();
+
+        torque = new Vector3(currDirection.z, 0, -currDirection.x);
     }
 
-    public void Init() {
-        
+    public void Init(Mesh _mesh) {
+        MeshCollider meshCollider = GetComponent<MeshCollider>();
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        meshCollider.sharedMesh = _mesh;
+        meshFilter.mesh = _mesh;
+
+        state = ParadeMemberState.Teleporting;
     }
 
     private void OnDrawGizmos() {
@@ -66,26 +72,32 @@ public class ParadeMember : MonoBehaviour {
         
         if (debug.showMemberTarget) {
             Gizmos.color = Color.blue;
-            Gizmos.DrawWireSphere(currentTarget, 0.75f);
+            Gizmos.DrawWireSphere(currTarget, 0.75f);
         }
         
         if (debug.showForwardVector) {
             Gizmos.color = Color.red;
             Gizmos.DrawLine(transform.position, transform.position + currDirection * 2f);
         }
+        
+        if (debug.showTorqueVector) {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + torque * 2f);
+        }
     }
 
     public void SetTarget(Vector3 targetPosition) {
-        currentTarget = targetPosition;
-        if (!agent.SetDestination(currentTarget)) {
-            Debug.LogWarning("Failed to set destination");
-        }
+        currTarget = targetPosition;
+        // if (!agent.SetDestination(currTarget)) {
+        //     Debug.LogWarning("Failed to set destination");
+        // }
     }
 }
 
 enum ParadeMemberState {
     Idle,
     Moving,
+    Teleporting
 }
 
 [Serializable]
@@ -93,4 +105,5 @@ public struct ParadeMemberDebug {
     public bool debugMode;
     public bool showMemberTarget;
     public bool showForwardVector;
+    public bool showTorqueVector;
 }
