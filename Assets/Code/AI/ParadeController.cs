8a66d78a475e65712860d370770d48167ac4da2d
tomgiagtz <tom.giagtzoglou@gmail.com>
2023-09-24 20:55:22 -0400

    navmesh solution :(

--- Assets/Code/AI/ParadeController.cs
+++ Assets/Code/AI/ParadeController.cs
@@ -6,9 +6,19 @@ using UnityEngine;
 public class ParadeController : MonoBehaviour {
 
     public Transform leader;
+    public float targetUpdateInterval = 1f;
+    
+    private List<ParadeMember> members = new List<ParadeMember>();
+    private Vector3 targetPosition = Vector3.zero;
+    
+    public ParadeControllerDebug debug;
+    
+    private void Start() {
+        InvokeRepeating("UpdateMemberPositions", 0, targetUpdateInterval);
+    }
 
-    private void Update() {
-        transform.position = leader.position;
+    private void FixedUpdate() {
+        targetPosition = leader.position;
     }
 
     public void AddMember(GameObject member) {
@@ -16,6 +26,33 @@ public class ParadeController : MonoBehaviour {
         
         Debug.Log("Adding member");
         ParadeMember newParadeMember = member.GetComponent<ParadeMember>();
+        members.Add(newParadeMember);
         newParadeMember.Init();
+        member.transform.SetParent(this.transform);
     }
+    
+    private void UpdateMemberPositions() {
+        for (int i = 0; i < members.Count; i++) {
+            members[i].SetTarget(targetPosition);
+        }
+    }
+    
+    
+    #region Debug
+    private void OnDrawGizmos() {
+        if (!debug.debugMode) return;
+        
+        if (debug.showParadeTarget) {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(targetPosition, 0.75f);
+        }
+    }
+    #endregion
+    
+    
+}
+[Serializable]
+public struct ParadeControllerDebug {
+    public bool debugMode;
+    public bool showParadeTarget;
 }
--- Assets/Code/AI/ParadeMember.cs
+++ Assets/Code/AI/ParadeMember.cs
@@ -2,24 +2,95 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ParadeMember : MonoBehaviour {
     
+    NavMeshAgent agent;
     Rigidbody rb;
-    MeshCollider coll;
+    
+    Vector3 currentTarget;
+    private Vector3 currDirection;
+    ParadeMemberState state = ParadeMemberState.Moving;
+    
+    
+    public ParadeMemberDebug debug;
     private void Start() {
         rb = GetComponent<Rigidbody>();
-        coll = GetComponent<MeshCollider>();
-        coll.isTrigger = true;
-        rb.isKinematic = true;
+        agent = GetComponent<NavMeshAgent>();
+        
+        agent.updatePosition = false;
+        agent.updateRotation = false;
+    }
+
+    private void FixedUpdate() {
+        // if (state == ParadeMemberState.Moving) {
+        //     rb.AddTorque(transform.right * 2500f, ForceMode.Force);
+        // }
+        // agent.transform.position = rb.position;
+        // if (!agent.isOnNavMesh) {
+        //     // agent.Warp(agent.nextPosition);
+        //     Debug.LogWarning("Agent not on navmesh");
+        // }
+        // agent.velocity = rb.velocity;
+        // if (agent.hasPath) {
+        //     // Get the next position on the path
+        //     Vector3 nextPosition = agent.steeringTarget;
+        //
+        //     // Calculate the direction to the next position
+        //     Vector3 moveDirection = (nextPosition - transform.position).normalized;
+        //     currDirection = moveDirection;
+        //     currDirection.y = 0f;
+        //     currDirection = currDirection.normalized;
+        //     
+        //     
+        //     rb.AddTorque(moveDirection * 10f, ForceMode.Impulse);
+        //     // Move the Rigidbody towards the next position
+        //     // rb.AddTorque(transform.right * Quaternion., ForceMode.Force);
+        //      agent.Warp(rb.position);
+        // } else {
+        //     // rb.position = agent.nextPosition;
+        // }
+        
+        if (state == ParadeMemberState.Moving) {
+               rb.AddTorque(transform.right * 2500f, ForceMode.Force);
+        }
     }
 
     public void Init() {
-        Debug.Log("Init Parade Member");
-        Destroy(gameObject.GetComponent<Collectable>());
-        coll.isTrigger = false;
-        rb.isKinematic = false;
+        
     }
-    
-    
+
+    private void OnDrawGizmos() {
+        if (!debug.debugMode) return;
+        
+        if (debug.showMemberTarget) {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(currentTarget, 0.75f);
+        }
+        
+        if (debug.showForwardVector) {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, transform.position + currDirection * 2f);
+        }
+    }
+
+    public void SetTarget(Vector3 targetPosition) {
+        currentTarget = targetPosition;
+        if (!agent.SetDestination(currentTarget)) {
+            Debug.LogWarning("Failed to set destination");
+        }
+    }
+}
+
+enum ParadeMemberState {
+    Idle,
+    Moving,
+}
+
+[Serializable]
+public struct ParadeMemberDebug {
+    public bool debugMode;
+    public bool showMemberTarget;
+    public bool showForwardVector;
 }
